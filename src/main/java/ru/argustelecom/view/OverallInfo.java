460428a32dde2232c13615b352451bce5602d708
Гришин Кирилл Вячеславович <k.grishin@voskhod.ru>
2020-07-19 23:36:40 +0300

    fixes

--- src/main/java/ru/argustelecom/view/OverallInfo.java
+++ src/main/java/ru/argustelecom/view/OverallInfo.java
@@ -77,6 +77,7 @@ public class OverallInfo implements Serializable {
     public void addFakeData() {
         if (emptyData) {
             dataMiner.bringItOn();
+            init();
         }
     }
 }
